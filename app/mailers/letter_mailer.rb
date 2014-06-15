99334e97d07f62c8b07b1856fb7294a4e414b09c
Anton Beloglazov <anton.beloglazov@gmail.com>
2014-06-15 11:46:23 +1000

    Added a sample implementation sending confirmation emails

--- app/mailers/letter_mailer.rb
+++ app/mailers/letter_mailer.rb
@@ -1,3 +1,14 @@
 class LetterMailer < ActionMailer::Base
-  default from: "from@example.com"
+    default from: "robot@openletter.cfapps.io"
+
+    # Subject can be set in your I18n file at config/locales/en.yml
+    # with the following lookup:
+    #
+    #   en.letter_mailer.confirmation_email.subject
+    #
+    def confirmation_email
+        @greeting = "Hi"
+        attachments['data.csv'] = File.read('db/data/StateRepsCSV.csv')
+        mail to: "anton.beloglazov@gmail.com"
+    end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,27 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# get Sendgrid credentials from ENV variable
+credentials = host = username = password = ''
+if !ENV['VCAP_SERVICES'].blank?
+    JSON.parse(ENV['VCAP_SERVICES']).each do |k,v|
+        if !k.scan("sendgrid").blank?
+            credentials = v.first.select {|k1,v1| k1 == "credentials"}["credentials"]
+            host = credentials["hostname"]
+            username = credentials["username"]
+            password = credentials["password"]
+        end
+    end
+end
+
+# overwrite ActionMailer settings to send through SendGrid servers
+ActionMailer::Base.smtp_settings = {
+    :address        => host,
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => username,
+    :password       => password,
+    :domain         => 'openletter.cfapps.io',
+    :enable_starttls_auto => true
+}
