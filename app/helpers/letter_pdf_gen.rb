a4ecb48ce16e907581f46f87a61ee855e2cd1f3c
Ermyas Abebe <ermyasabebe@Ermyass-MacBook-Pro.local>
2014-06-15 11:23:03 +1000

    bold 'regarding' section r Please enter the commit message for your changes. Lines starting

--- app/helpers/letter_pdf_gen.rb
+++ app/helpers/letter_pdf_gen.rb
@@ -17,14 +17,19 @@ def generate_pdf(from, to, content, footer, regarding, output_file)
 
   def to(pdf, to)
       generate_bb(pdf, -5, to, 30, 170, 50, 0 )
-
   end
   def reg(pdf, regarding)
-      generate_bb(pdf, -30, regarding, 30, 170, 50, 0 )
+      # generate_bb(pdf, -30, regarding, 30 , 170, 50, 0 )
+      # TODO: figure out a way to pass the style of a text and refactor this out to generate_bb
+      y_position = pdf.cursor - 30
+      pdf.bounding_box([30, y_position], :width => 170, :height => 50) do
+          pdf.transparent(0) { stroke_bounds }
+          text regarding, :align=>:left, :style => :bold
+      end
   end
 
   def body(pdf, content)
-    generate_bb(pdf, 10, content, 30,470, 400, 0)
+      generate_bb(pdf, 10, content, 30,470, 300, 0)
   end
 
   def footer(pdf, footer)
@@ -35,7 +40,7 @@ def generate_pdf(from, to, content, footer, regarding, output_file)
       y_position = pdf.cursor + cursor_offset
       pdf.bounding_box([left, y_position], :width => width, :height => height) do
           pdf.transparent(stroke_opacity) { stroke_bounds }
-          text text, :align=>:justify
+          text text, :align=>:left
       end
   end
      Prawn::Document.generate(output_file, :page_size =>'A4') do
@@ -49,6 +54,6 @@ end
 
 # from commandline
 if(__FILE__ == $0)
-  generate_pdf("Mr. Kent Steer\n406B Altona Nort, 3011\nPhone:0413627643", "Mr. Kent Steer\n406B Altona Nort, 3011\nPhone:0413627643", "Dear Minister,\n\n" + ("The quick brown fox jumps over the lazy dog.  The quick brown fox jumps over the lazy dog." *25), "Sincerely Yours,\nErmyas Abebe", "RE: Pot holes in Altona North", ARGV[0])
+    generate_pdf("Mr. Kent Steer\n406B Altona Nort, 3011\nPhone: 0413627643", "Mr. Kent Steer\n406B Altona Nort, 3011\nPhone: 0413627643", "Dear Minister,\n\n" + ("The quick brown fox jumps over the lazy dog.  The quick brown fox jumps over the lazy dog." *15), "Sincerely Yours,\nErmyas Abebe", "RE: Pot holes in Altona North", ARGV[0])
 end
 
